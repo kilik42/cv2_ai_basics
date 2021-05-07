cb8c58040f4d9e78a1839a1a49faaf14495cc6e8
kilik42 <marvinevins@yahoo.com>
2021-05-07 14:16:21 -0500

    working on guassian blur

--- basics.py
+++ basics.py
@@ -21,16 +21,26 @@ print("package imported")
 
 
 # read webcam
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)#width
-cap.set(4, 480)#height
-cap.set(10,100) #brightness of webcam
+# cap = cv2.VideoCapture(0)
+# cap.set(3, 640)#width
+# cap.set(4, 480)#height
+# cap.set(10,100) #brightness of webcam
+#
+# while True:
+#     #will save  image into img and tell if it was sucess or not
+#     success, img = cap.read()
+#     cv2.imshow("Video", img)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
 
-while True:
-    #will save  image into img and tell if it was sucess or not
-    success, img = cap.read()
-    cv2.imshow("Video", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+#basic functions when building open cv projects
+img = cv2.imread("resources/images/lion.jpg")
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray, (7,7), 0)
+cv2.imshow("Gray image", imgGray)
+cv2.imshow("Blurred image", imgBlur)
+cv2.waitKey(0)
+
+
 
 
